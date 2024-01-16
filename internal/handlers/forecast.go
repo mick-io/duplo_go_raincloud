3d3bf5bf8ebc1e2432ecb90a5cc0232b101ad30d
Mick Piereder <mick.piereder@carruslearn.com>
2024-01-15 18:27:25 -0700

    Add fetching and storing latest forecast for multiple locations

--- internal/handlers/forecast.go
+++ internal/handlers/forecast.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
@@ -107,7 +109,70 @@ func ReadStoredForecast(db database.Datastore) echo.HandlerFunc {
 
 func ReadLatestForecast(db database.Datastore, WeatherAPIClient api.WeatherAPIClient) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		// TODO: Fetch and upsert latest forecast
+		locations := []models.LocationRecord{}
+		if err := db.Find(&locations); err != nil {
+			msg := fmt.Sprintf("Error querying database: %v", err)
+			return echo.NewHTTPError(http.StatusInternalServerError, msg)
+		}
+
+		var wg sync.WaitGroup
+		wg.Add(len(locations))
+		errs := make(chan error, len(locations))
+
+		for _, location := range locations {
+			go func(location models.LocationRecord) {
+				// Fetching forecast data
+				resp := models.Forecast{}
+				opts := api.ForecastOptions{
+					Latitude:  strconv.FormatFloat(location.Latitude, 'f', 6, 64),
+					Longitude: strconv.FormatFloat(location.Longitude, 'f', 6, 64),
+				}
+				if err := WeatherAPIClient.GetForecast(opts, &resp); err != nil {
+					msg := fmt.Sprintf("Error getting forecast: %v", err)
+					errs <- echo.NewHTTPError(http.StatusBadGateway, msg)
+				}
+
+				// Validating forecast data
+				if err := models.ValidateForecast(resp); err != nil {
+					msg := fmt.Sprintf("Error validating forecast: %v", err)
+					errs <- echo.NewHTTPError(http.StatusBadGateway, msg)
+				}
+
+				// Storing forecast data
+				forecast := models.NewForecastRecords(location.ID, &resp)
+				if err := db.Save(forecast); err != nil {
+					msg := fmt.Sprintf("Error storing forecast: %v", err)
+					errs <- echo.NewHTTPError(http.StatusInternalServerError, msg)
+				}
+				hourly := models.NewHourlyRecord(forecast.Model.ID, &resp.Hourly)
+				if err := db.Create(&hourly); err != nil {
+					msg := fmt.Sprintf("Error storing hourly records: %v", err)
+					errs <- echo.NewHTTPError(http.StatusInternalServerError, msg)
+				}
+				units := models.NewHourlyUnitsRecord(forecast.Model.ID, &resp.HourlyUnits)
+				if err := db.Save(units); err != nil {
+					msg := fmt.Sprintf("Error storing unit records: %v", err)
+					errs <- echo.NewHTTPError(http.StatusInternalServerError, msg)
+				}
+
+			}(location)
+		}
+
+		go func() {
+			wg.Wait()
+			close(errs)
+		}()
+
+		select {
+		case err := <-errs:
+			if err != nil {
+				return err
+			}
+		case <-time.After(time.Second * 10):
+			msg := "Timeout waiting for goroutines to finish"
+			return echo.NewHTTPError(http.StatusInternalServerError, msg)
+		}
+
 		return ReadStoredForecast(db)(c)
 	}
 }
